c1f3075aa9688ffdcdeda4ae7aa93e9629f8fa6d
user12043 <user12043@hotmail.com>
2021-09-14 18:49:47 +0300

    thresholding start

---
+++ 07thresholding.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# created by user12043 on 14.09.021 - 16:10
+# part of project opencv-tutorial
+
+# https://docs.opencv.org/4.5.3/d7/d4d/tutorial_py_thresholding.html
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# SIMPLE THRESHOLDING
+
+# Input should be gray-scale image
+# img_color = cv.imread("img/black_white_gradiant.png")
+# img = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+img = cv.imread("img/black_white_gradiant.png", 0)
+img = cv.resize(img, None, fx=0.5, fy=0.5)
+
+ret, thresh1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+ret, thresh2 = cv.threshold(img, 127, 255, cv.THRESH_BINARY_INV)
+ret, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
+ret, thresh4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
+ret, thresh5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
+
+titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+for i in range(6):
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray', vmin=0, vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.show()
+
+# ADAPTIVE THRESHOLDING
+
+img = cv.imread("img/sudoku.jpg", 0)
+# Notice the median blur before
+img = cv.medianBlur(img, 5)
+mean_threshold = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+gauss_threshold = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+plt.figure()
+plt.subplot(1, 2, 1), plt.imshow(mean_threshold, 'gray')
+plt.title("MEAN_THRESHOLD")
+plt.subplot(1, 2, 2), plt.imshow(gauss_threshold, 'gray')
+plt.title("GAUSSIAN_THRESHOLD")
+
+plt.show()
