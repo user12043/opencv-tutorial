b9ad550622b7554843e13d2c7086409b7ac15ebe
user12043 <user12043@hotmail.com>
2021-09-24 10:31:27 +0300

    thresholding end

--- 07thresholding.py
+++ 07thresholding.py
@@ -7,7 +7,6 @@
 # https://docs.opencv.org/4.5.3/d7/d4d/tutorial_py_thresholding.html
 
 import cv2 as cv
-import numpy as np
 import matplotlib.pyplot as plt
 
 # SIMPLE THRESHOLDING
@@ -24,13 +23,13 @@ ret, thresh3 = cv.threshold(img, 127, 255, cv.THRESH_TRUNC)
 ret, thresh4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 ret, thresh5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
 
-titles = ['Original Image', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+titles = ["Original Image", "BINARY", "BINARY_INV", "TRUNC", "TOZERO", "TOZERO_INV"]
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 for i in range(6):
-    plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray', vmin=0, vmax=255)
+    plt.subplot(2, 3, i + 1), plt.imshow(images[i], "gray", vmin=0, vmax=255)
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.show()
 
 # ADAPTIVE THRESHOLDING
 
@@ -41,9 +40,24 @@ mean_threshold = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.TH
 gauss_threshold = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
 
 plt.figure()
-plt.subplot(1, 2, 1), plt.imshow(mean_threshold, 'gray')
+plt.subplot(1, 2, 1), plt.imshow(mean_threshold, "gray")
 plt.title("MEAN_THRESHOLD")
-plt.subplot(1, 2, 2), plt.imshow(gauss_threshold, 'gray')
+plt.subplot(1, 2, 2), plt.imshow(gauss_threshold, "gray")
 plt.title("GAUSSIAN_THRESHOLD")
 
+# plt.show()
+
+# OTSU
+plt.figure()
+img = cv.imread("img/noisy2.png", 0)
+ret1, thresh1 = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+plt.subplot(2, 2, 1), plt.imshow(thresh1, "gray")
+plt.title("OTSU")
+plt.subplot(2, 2, 2), plt.hist(thresh1.ravel(), 256)
+
+blur = cv.GaussianBlur(img, (5, 5), 0)
+ret2, thresh2 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+plt.subplot(2, 2, 3), plt.imshow(thresh2, "gray")
+plt.title("OTSU with Blur")
+plt.subplot(2, 2, 4), plt.hist(thresh2.ravel(), 256)
 plt.show()
